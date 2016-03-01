240f495743e14180bddca701fa9d8d8968dd2cca
ingolf heinsch <ingolf@nsynk.de>
2016-03-01 11:37:06 +0100

    fix cs project file

--- plugins/DX11Player/DX11Player/Properties/AssemblyInfo.cs
+++ plugins/DX11Player/DX11Player/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DX11Player")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0dd76e97-e63a-48c0-8bee-2bf91874f887")]
+[assembly: Guid("ad7ad4ed-8019-48c4-babe-6d77a5ffe074")]
 
 // Version information for an assembly consists of the following four values:
 //
