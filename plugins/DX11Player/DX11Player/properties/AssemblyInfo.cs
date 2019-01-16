6a60741c6d4f6a10170641ca6a652918a75d11bc
Guido Schmidt <guido@nsynk.de>
2019-01-16 17:42:17 +0100

    EDIT/plugins/DX11Player/properties/AssemblyInfo.cs: bump version

--- plugins/DX11Player/DX11Player/properties/AssemblyInfo.cs
+++ plugins/DX11Player/DX11Player/properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("NOVERSION")]
+[assembly: AssemblyInformationalVersion("1.0.0-0-g1465a7f")]
